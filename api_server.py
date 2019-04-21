9d6d2b9e63e91675068751eb8851b36162df6a41
hayashi <sshayashi0208@gmail.com>
2019-04-21 18:16:41 +0900

    add fiels

--- api_server.py
+++ api_server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import jsonify
+from flask import request
+from sqlalchemy import exc
 
 db = SQLAlchemy()
 
@@ -18,6 +20,17 @@ def create_app():
 
 app = create_app()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String, unique=True)
+    password = db.Column(db.String(20), nullable=False)
+    nickname = db.Column(db.String(80), nullable=True)
+    comment = db.Column(db.String(100), default='')
+
+    def __repr__(self):
+        return '<User %r>' % self.user_id
+
+
 @app.route('/')
 def hello_world():
     from model import User
@@ -27,6 +40,38 @@ def hello_world():
         'name': a.username
     })
 
+
+def validation(json_data):
+    pass
+@app.route('/signup', methods=["POST"])
+def signup():
+    data = request.get_json()
+    #validaiton
+    user_id = data['user_id']
+    password = data['password']
+    u = User(user_id=user_id, password=password)
+    try:
+        db.session.add(u)
+        db.session.commit()
+        return jsonify({
+            "message": "Account successfully created",
+            "user": data
+        }, 200)
+    except Exception as e:
+        return jsonify({
+            "message": "Account creation failed",
+            "cause": str(e)}, 400)
+
+
+@app.route('/users/<string:user_id>')
+def users(user_id):
+    user = User.query.filter_by(user_id=user_id).first()
+    return 'users'
+
+
+@app.route('/close')
+def close():
+    return 'close'
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 from api_server import db
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    user_id = db.Column(db.String, unique=True)
+    nickname = db.Column(db.String(80), unique=True, nullable=True)
+    password = db.Column(db.String(20), nullable=False)
+    comment = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<User %r>' % self.user_id
